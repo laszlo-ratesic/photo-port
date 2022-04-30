0a2f0b73b1636f4b4537b863ba97e9242380bd86
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-30 10:59:46 -0500

    write tests for Nav component

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    // baseline test
+    it('renders', () => {
+        render(<Nav />);
+    });
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
+
+/*
+**NOTE: additional describe is not imperative
+for test to run; it is used to organize test
+into sections with labels for element tested
+*/
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        // Arrange
+        render(<Nav />);
+        // Assert test using custom matcher
+        // Queries aria-label and tests for text content
+        // Pros: 2-fold test for accessibility features as well as emoji
+        expect(screen.getByLabelText('camera')).toHaveTextContent('📸');
+    });
+})
+
+describe('links are visibile', () => {
+    it('inserts text into the links',() => {
+        // Arrange
+        render(<Nav />);
+        // Assert
+        expect(screen.getByTestId("link")).toHaveTextContent('Oh Snap!');
+        expect(screen.getByTestId("about")).toHaveTextContent('About me');
+    })
+})
