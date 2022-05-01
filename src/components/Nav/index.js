ba6638b29117d79eb5e13d84a994db5cba5e4cac
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-30 19:15:00 -0500

    render document title

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-
   const {
     categories = [],
     setCurrentCategory,
     currentCategory
   } = props;
 
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
