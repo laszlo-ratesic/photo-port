0fbf13fbdb19b50d04888c9584f441b1496b7b6e
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-05-01 21:31:24 -0500

    add mocks to Nav tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,45 +1,75 @@
-import React from 'react';
-import { render, cleanup, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-        render(<Nav />);
-    });
-    // snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
-        // assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-    });
-})
+const categories = [
+  { name: "portraits", description: "Portraits of people in my life" },
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
+describe("Nav component", () => {
+  // baseline test
+  it("renders", () => {
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+  });
+  // snapshot test
+  it("matches snapshot", () => {
+    const { asFragment } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
 
 /*
 **NOTE: additional describe is not imperative
 for test to run; it is used to organize test
 into sections with labels for element tested
 */
-describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-        // Arrange
-        render(<Nav />);
-        // Assert test using custom matcher
-        // Queries aria-label and tests for text content
-        // Pros: 2-fold test for accessibility features as well as emoji
-        expect(screen.getByLabelText('camera')).toHaveTextContent('📸');
-    });
-})
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    // Arrange
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // Assert test using custom matcher
+    // Queries aria-label and tests for text content
+    // Pros: 2-fold test for accessibility features as well as emoji
+    expect(screen.getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
 
-describe('links are visibile', () => {
-    it('inserts text into the links',() => {
-        // Arrange
-        render(<Nav />);
-        // Assert
-        expect(screen.getByTestId("link")).toHaveTextContent('Oh Snap!');
-        expect(screen.getByTestId("about")).toHaveTextContent('About me');
-    })
-})
+describe("links are visibile", () => {
+  it("inserts text into the links", () => {
+    // Arrange
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // Assert
+    expect(screen.getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(screen.getByTestId("about")).toHaveTextContent("About me");
+  });
+});
