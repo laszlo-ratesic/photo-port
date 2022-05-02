054a8c5d5732b911f12d86d117ee19e3cab7ca45
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-05-01 21:59:31 -0500

    add gallery and photolist unit tests

---
+++ src/components/PhotoList/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PhotoList from "..";
+
+afterEach(cleanup);
+
+describe("PhotoList renders", () => {
+  it("renders", () => {
+    render(<PhotoList />);
+  });
+  it("matches snapshot", () => {
+    const { asFragment } = render(<PhotoList category="portrait" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
