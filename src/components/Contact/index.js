f09e031b0c3c17e092ae1c0da544b69a71b52ede
laszlo-ratesic <keenanrc@hotmail.com>
2022-05-03 08:46:00 -0500

    sync form state with user input

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
-  <section>
-    <h1>Contact Me</h1>
-    <form id="contact-form">
-      {/* Name */}
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
-      </div>
-      {/* Email */}
-      <div>
-        <label htmlFor="email">Email address:</label>
-        <input type="email" name="email" />
-      </div>
-      {/* Message Text */}
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea name="message" rows="5" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  </section>;
+  const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+      setFormState({...formState, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <form id="contact-form">
+        {/* Name */}
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+        </div>
+        {/* Email */}
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+        </div>
+        {/* Message Text */}
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
